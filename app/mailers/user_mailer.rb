7bda189776e1b3859093b847f6e7e981e4e64d5c
App Academy Student <student@appacademy.io>
2014-02-21 19:40:48 -0800

    Fixed mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def activation_email(user)
     @user = user
-    @url = activate_users_url(activation_token: @user.activation_token)
+    @url = "localhost:3000/users/activate?activation_token=#{ @user.activation_token}"
     mail(to: user.email, subject: "Activate Your Account")
   end
 end
